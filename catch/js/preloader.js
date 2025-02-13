12c408ac50e51fae8f6818033133696df4cbab39
ThAtBlOob <143834475+ThAtBlOob@users.noreply.github.com>
2025-02-12 18:49:04 -0800

    add new assets

--- catch/js/preloader.js
+++ catch/js/preloader.js
@@ -11,6 +11,6 @@ export default class Preloader extends Phaser.Scene
         this.loadText = this.add.text(512, 360, 'Loading ...', { fontFamily: 'Arial', fontSize: 74, color: '#e3f2ed' });
         this.loadText.setOrigin(0.5);
         this.load.setPath('./assets');
-        this.load.image('ashwanth');
+        this.load.image(['spacey','ihangry']);
     }
 }
