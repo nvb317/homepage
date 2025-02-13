84f7664e24b5922f18d09cfed8fcb69871e24658
ThAtBlOob <143834475+ThAtBlOob@users.noreply.github.com>
2025-02-13 19:22:31 +0000

    Update game.js
    Add multiple items to collect

--- catch/js/game.js
+++ catch/js/game.js
@@ -16,15 +16,21 @@ export default class Game extends Phaser.Scene
     create() {
         this.player = new Player(this,512,670,"ihangry");
         this.player.setScale(0.6);
-        this.item = new Items(this,512,0,"spacey");
-        this.item.setScale(0.06)
         this.player.setGravityY(-200);
         this.player.setCollideWorldBounds(true);
         this.player.setBounce(0);
         this.player.velocity = 0;
+        
+        this.item = this.physics.add.group({
+            key: 'spacey',
+            repeat: 3,
+        });
+
+        this.item.setScale(0.06);
+
         this.cursor = this.input.keyboard.createCursorKeys();
         this.scoretext = this.add.text(20, 20, 'Score: 0', { fontFamily: 'Arial', fontSize: 40, color: '#e3f2ed' });
-        this.physics.add.overlap(this.player, this.item, (player, item) => this.collect());
+        this.physics.add.overlap(this.player, this.item, this.collect,null, this);
     }
     update (){
         if (this.cursor.left.isDown)
@@ -37,18 +43,23 @@ export default class Game extends Phaser.Scene
         }
         this.player.setVelocityX(this.player.velocity);
         this.player.velocity *= 0.95;
+        
+        this.item.children.iterate(child =>
+        {
+            if(child.y > 700) {
+                child.y = -10;
+                child.x = Math.random() * 1000;
+                child.setVelocityY(0);
+            }
+        });
 
-        if(this.item.y > 700) {
-            this.item.y = -10;
-            this.item.x = Math.random() * 1000;
-            this.item.setVelocityY(0);
-        }
     }
-    collect() {
+    collect(player, item) {
         this.score += 1;
-        this.item.y = -10;
-        this.item.x = Math.random() * 1000;
-        this.item.setVelocityY(0);
         this.scoretext.setText('Score: ' + this.score);
+        
+        item.y = -10;
+        item.x = Math.random() * 1000;
+        item.setVelocityY(0);
     }
 }
