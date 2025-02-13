e19c934bb781c6aafa534fdc42924dd1b1a17eef
ThAtBlOob <143834475+ThAtBlOob@users.noreply.github.com>
2025-02-13 07:32:26 -0800

    Delete catch directory

--- catch/js/items.js
+++
@@ -1,7 +0,0 @@
-export default class Items extends Phaser.Physics.Arcade.Image
-{
-    constructor(scene,x,y,texture) {
-        super(scene,x,y,texture);
-        
-    }
-}
--- catch/js/player.js
+++
@@ -1,7 +0,0 @@
-export default class Player extends Phaser.Physics.Arcade.Image
-{
-    constructor(scene,x,y,texture) {
-        super(scene,x,y,texture);
-        self.setGravity(0,0);
-    }
-}
