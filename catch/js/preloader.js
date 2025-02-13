c086cb6429943ba800965585223db2af12634407
ThAtBlOob <143834475+ThAtBlOob@users.noreply.github.com>
2025-02-12 18:30:19 -0800

    edit preloader

--- catch/js/preloader.js
+++ catch/js/preloader.js
@@ -12,7 +12,7 @@ export default class Preloader extends Phaser.Scene
         this.loadText.setOrigin(0.5);
         this.loadText.setStroke('#203c5b', 6);
         this.loadText.setShadow(2, 2, '#2d2d2d', 4, true, false);
-        this.load.setPath('../assets');
+        this.load.setPath('./assets');
         this.load.image('ashwanth');
     }
 }
